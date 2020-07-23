81b7812b593ae1f30f57eb7bca5c58de553e1441
Marius Olariu <marius.lucian.olariu@gmail.com>
2020-07-23 22:50:17 +0100

    removed src folder

--- src/components/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Layout({ children }) {
-  return (
-    <div style={{ maxWidth: 1000, margin: '1.7em auto', background: "white", overflow: 'hidden', fontSize: 18 + "px" }}>
-      {children}
-    </ div>
-  );
-}

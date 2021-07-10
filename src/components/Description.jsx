e03457764181b8b76fe4ca6b613ee12f1c5764e4
Marius Olariu <marius.olariu@diffblue.com>
2021-07-10 10:10:07 +0100

    Added extra skills dr.Cozma

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -86,7 +86,9 @@ export default function Description() {
           <div style={{ float: "right" }}>
             Medic Primar Cardiolog<br />
             Institutul de Boli Cardiovasculare Timișoara<br />
-            Competență în Electrofiziologie <br />
+            Competență în Electrofiziologie<br />
+            Competență în Pacing-Stimulatoare<br />
+            Competență în Defibrilatoare Cardiace<br />
             Competență în Ecocardiografie<br />
           </div>
 

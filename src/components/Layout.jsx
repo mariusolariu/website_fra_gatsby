5ebfe17a124c4c42d01bd38a019c650ff3ebc7bb
Marius Olariu <marius.lucian.olariu@gmail.com>
2021-08-29 21:59:34 +0100

    Put abbreviate name and decrease font size

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Layout({ children }) {
   return (
-    <div style={{ maxWidth: 1000, margin: '1.2em auto', background: "white", overflow: 'hidden', fontSize: 18 + "px" }}>
+    <div style={{ maxWidth: 1000, margin: '1.2em auto', background: "white", overflow: 'hidden', fontSize: 16 + "px" }}>
       {children}
     </ div>
   );

583780bb81e38cdfccefce62bcde9157fc7e006c
Marius Olariu <marius.olariu@imperatives.co.uk>
2026-04-27 14:04:03 +0100

    add logo

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 import { PhoneIcon } from "./Icons";
 import AccessibilityMenu from "./AccessibilityMenu";
 
 export default function Topbar() {
+  const data = useStaticQuery(graphql`
+    query TopbarAvatarQuery {
+      file(relativePath: { eq: "avatar.png" }) {
+        childImageSharp {
+          fluid(
+            maxWidth: 120
+            quality: 72
+            srcSetBreakpoints: [44, 52, 88, 104]
+          ) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
+  const avatar = data.file.childImageSharp.fluid;
+
   return (
     <header className="topbar">
       <div className="topbar-inner">
         <a href="#main" className="brand" aria-label="Cabinet Cardiologie Dr. Olariu Ioan">
-          <div className="brand-mark">O</div>
+          <div className="brand-mark">
+            <Img fluid={avatar} alt="" loading="eager" fadeIn={false} />
+          </div>
           <div className="brand-text">
             <span className="b1">Dr. Olariu Ioan</span>
             <span className="b2">Cabinet de Cardiologie · Bocșa</span>
